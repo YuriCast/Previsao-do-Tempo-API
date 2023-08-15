a2f0c9b8911091ec5b1b44a0f064b7c5c9dbe7f8
Yuri Castilho <yuricastilho2005@gmail.com>
2023-08-15 14:45:03 -0300

    componente de pesquisa criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, ChangeEvent, useEffect } from 'react'
 
 import { optionType } from './types'
+import Search from './components/Search'
 
 const App = (): JSX.Element => {
   const [term, setTerm] = useState<string>('')
@@ -55,41 +56,13 @@ const getSearchOptions = (value: string) => {
 
   return (
   <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-    <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg text-zinc-700">
-
-      <h1 className="text-4xl font-thin">
-        Previsão do <span className="font-black">Tempo</span>
-      </h1>
-
-      <p className="text-sm mt-2">
-      Digite o local do qual você deseja saber o clima e selecione uma opção no menu
-      </p>
-
-      <div className="relative flex mt-10 md:mt-4">
-        <input 
-          type="text" 
-          value={term}
-          className="px-2 py-1 rounded-1md border-2 border-white"
-          onChange={onInputChange}
-        />
-
-        <ul className='absolute top-9 bg-white ml-1 rounded-b-md'>
-          {options.map((option: optionType, index : number) => (
-            <li key={option.name + '-' + index}>
-              <button className='text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer'
-              onClick={() => onOptionSelect(option)}>
-                {option.name}
-              </button>
-            </li>
-          ))}
-        </ul>
-
-        <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer"
-        onClick={onSubmit}>
-          procurar
-        </button>
-      </div>
-    </section>
+    <Search 
+      term={term}
+      options={options}
+      onInputChange={onInputChange}
+      onOptionSelect={onOptionSelect}
+      onSubmit={onSubmit}
+    />
   </main>
   )
 }

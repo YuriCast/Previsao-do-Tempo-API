d5ab8bb9f53780d42aa7913e21f28cd5cd7e113b
Yuri Castilho <yuricastilho2005@gmail.com>
2023-08-14 23:00:17 -0300

    introduzindo a weather-api

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
+import { useState } from 'react'
+
 const App = () => {
+  const [term, setTerm] = useState('')
+
+const onInputChange = (e) => {
+  console.log(e.target.value)
+}
+
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
   return (
   <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
     <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg text-zinc-700">
@@ -14,8 +24,9 @@ const App = () => {
       <div className="flex mt-10 md:mt-4">
         <input 
           type="text" 
-          value={'k'}
+          value={term}
           className="px-2 py-1 rounded-1md border-2 border-white"
+          onChange={onInputChange}
         />
 
         <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer">
